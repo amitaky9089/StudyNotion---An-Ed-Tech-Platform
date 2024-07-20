e8c4ef78925a314d63344448ebfe8a159d143a15
Amit Kumar Yadav <2021uee0129@iitjammu.ac.in>
2024-07-20 12:47:30 +0530

    second message

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./reducer";
-
+import { Toaster } from "react-hot-toast";
 const store = configureStore({
   reducer: rootReducer,
 });
@@ -17,6 +17,7 @@ root.render(
     <Provider store={store}>
     <BrowserRouter>
         <App />
+        <Toaster/>
     </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,16 +1,16 @@
 import { combineReducers } from "@reduxjs/toolkit"
 
 import authReducer from "../slices/authSlice"
-// import cartReducer from "../slices/cartSlice"
+import cartReducer from "../slices/cartSlice"
 // import courseReducer from "../slices/courseSlice"
-// import profileReducer from "../slices/profileSlice"
+import profileReducer from "../slices/profileSlice"
 // import viewCourseReducer from "../slices/viewCourseSlice"
 
 const rootReducer = combineReducers({
   auth: authReducer,
-//   profile: profileReducer,
+  profile: profileReducer,
 //   course: courseReducer,
-//   cart: cartReducer,
+  cart: cartReducer,
 //   viewCourse: viewCourseReducer,
 })
 
