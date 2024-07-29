ed3e405ea276d2801996e4864f784f3cec795696
Amit Kumar Yadav <2021uee0129@iitjammu.ac.in>
2024-07-29 18:43:50 +0530

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
-import { Route,Routes } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+// React Router
+import { Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -10,65 +14,128 @@ import UpdatePassword from "./pages/UpdatePassword";
 import VerifyEmail from "./pages/VerifyEmail";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Error from "./pages/Error";
+import Settings from "./components/core/Dashboard/Settings";
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
+import Cart from "./components/core/Dashboard/Cart";
+import AddCourse from "./components/core/Dashboard/AddCourse";
+import { ACCOUNT_TYPE } from "./utils/constants";
 
 function App() {
+  const { user } = useSelector((state) => state.profile);
+
   return (
-     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
-        <Navbar/>  
-        <Routes>
-           <Route path="/" element={<Home/>}/>
+    <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
 
-           <Route path="login" element={
+        <Route
+          path="login"
+          element={
             <OpenRoute>
-                  <Login/>
+              <Login />
             </OpenRoute>
-            }/>
+          }
+        />
 
-           <Route path="signup" element={
+        <Route
+          path="signup"
+          element={
             <OpenRoute>
-                 <SignUp/>
+              <SignUp />
             </OpenRoute>
-         }
-            />
+          }
+        />
 
-             <Route path="forgot-password" element={
+        <Route
+          path="forgot-password"
+          element={
             <OpenRoute>
-                 <ForgotPassword/>
+              <ForgotPassword />
             </OpenRoute>
-         }
-            />
+          }
+        />
 
-           {/* as token is also present in route therefore :id is required  */}
-          <Route path="update-password/:id" element={
+        {/* as token is also present in route therefore :id is required  */}
+        <Route
+          path="update-password/:id"
+          element={
             <OpenRoute>
-                 <UpdatePassword/>
+              <UpdatePassword />
             </OpenRoute>
-         }
-            />
+          }
+        />
 
-            <Route path="verify-email" element={
+        <Route
+          path="verify-email"
+          element={
             <OpenRoute>
-                 <VerifyEmail/>
+              <VerifyEmail />
             </OpenRoute>
-         }
-            />
+          }
+        />
 
-         <Route path="about" element={
+        <Route
+          path="about"
+          element={
             <OpenRoute>
-                 <About/>
+              <About />
             </OpenRoute>
-         }
-            />
+          }
+        />
 
-         <Route path="contact" element={
+        <Route
+          path="contact"
+          element={
             <OpenRoute>
-                 <Contact/>
+              <Contact />
             </OpenRoute>
-         }
-            />
-        </Routes>
-     </div>
+          }
+        />
+
+        <Route
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        >
+          {/* Routes for all users  */}
+          <Route path="dashboard/my-profile" element={<MyProfile />} />
+          <Route path="dashboard/settings" element={<Settings />} />
+
+          {/* Route only for Instructors */}
+          {user?.accountType === ACCOUNT_TYPE.INSTRUCTOR && (
+            <>
+              {/* <Route path="dashboard/instructor" element={<Instructor />} /> */}
+              {/* <Route path="dashboard/my-courses" element={<MyCourses />} /> */}
+              <Route path="dashboard/add-course" element={<AddCourse />} />
+              {/* <Route
+                path="dashboard/edit-course/:courseId"
+                element={<EditCourse />}
+              /> */}
+            </>
+          )}
+
+          {/* Route only for Students */}
+          {user?.accountType === ACCOUNT_TYPE.STUDENT && (
+            <>
+              <Route
+                path="dashboard/enrolled-courses"
+                element={<EnrolledCourses />}
+              />
+              <Route path="/dashboard/cart" element={<Cart />} />
+            </>
+          )}
+        </Route>
+
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </div>
   );
 }
 
